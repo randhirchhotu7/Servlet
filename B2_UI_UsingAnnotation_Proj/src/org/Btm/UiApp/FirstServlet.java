4b8903a02f6ac555a30fb1a576eceb9555561197
Randhir Prakash <61622146+randhirchhotu7@users.noreply.github.com>
2021-03-28 16:36:43 +0530

    Add files via upload

---
+++ B2_UI_UsingAnnotation_Proj/src/org/Btm/UiApp/FirstServlet.java
@@ -0,0 +1,25 @@
+package org.Btm.UiApp;
+import javax.servlet.annotation.*;
+import java.io.*;
+import javax.servlet.GenericServlet;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+@WebServlet("/btm")    //from form page//
+public class FirstServlet extends GenericServlet 
+{
+
+	@Override
+	public void service(ServletRequest req, ServletResponse resp) throws ServletException, IOException 
+	{
+		String name = req.getParameter("nm");
+		String place = req.getParameter("pl");
+
+		PrintWriter out = resp.getWriter();
+		out.println("<html><body bgcolor='Yellow'>" + "<h1>New member of our class is " + name + " from " + place
+				+ " !!" + "</h1>" + "</body></html>");
+		out.close();
+
+	}
+
+}
---
+++ C2_ServletCycleUsingLoadOnwebXml_proj2/src/org/btm/ServletCycle_App2/FirstServlet.java
@@ -0,0 +1,44 @@
+package org.btm.ServletCycle_App2;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.GenericServlet;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+
+public class FirstServlet extends GenericServlet
+{
+	public FirstServlet() 
+	{
+		System.out.println("Servlet object is created !!");
+	}
+	@Override
+	public void init() throws ServletException 
+	{
+//		super.init();
+		System.out.println("Servlet object is Initialized !!");
+	}
+	
+	@Override
+	public void service(ServletRequest req, ServletResponse resp) throws ServletException, IOException 
+	{
+		String name = req.getParameter("nm");
+		String fName = req.getParameter("fnm");
+		String place = req.getParameter("pl");
+		PrintWriter out = resp.getWriter();
+		out.println("<html><body bgcolor='yellow'>"
+				+ "<h1>New Student of BTM is : "+name+"<br></br>"+"Father Name is : "+fName+"<br></br>"+"Home State is : "+place
+				+ "</h1>"+"</body></html>");
+		out.close();
+		System.out.println("Service method is Executed !!");
+	}
+	
+	@Override
+	public void destroy() 
+	{
+//		super.destroy();
+		System.out.println("Closed all costly resources !!");
+	}
+}
